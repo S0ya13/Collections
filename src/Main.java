dad21fbc1be44d9f5b5ed2e6d8e7a8ffc3aa8cf3
Soya <oj.savchak@gmail.com>
2025-12-10 17:35:16 +0100

    More beautiful display text in CLI.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,22 @@
 void main() {
+    System.out.println("Task 1 start");
     Task1 task1 = new Task1();
     task1.main(new String[0]);
+    System.out.println("Task 1 end\n");
 
+    System.out.println("Task 2 start");
     Task2 task2 = new Task2();
     task2.main(new String[0]);
+    System.out.println("Task 2 end\n");
 
+    System.out.println("Task 3 start");
     Task3 task3 = new Task3();
     task3.main(new String[0]);
+    System.out.println("Task 3 end\n");
 
+    System.out.println("Task 4 start");
     Task4 task4 = new Task4();
     task4.main(new String[0]);
+    System.out.println("Task 4 end\n");
+
 }
